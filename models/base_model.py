274097d890ccc26e4be45305a39ecf8a0b2f502d
Sebastian <sebaslopez.2002@gmail.com>
2022-11-29 18:37:47 -0500

    fixed bug of id creation

--- models/base_model.py
+++ models/base_model.py
@@ -19,16 +19,16 @@ class BaseModel:
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
-        if not kwargs:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
-        else:
+
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.utcnow()
+        self.updated_at = datetime.utcnow()
+        if kwargs:
             for key, value in kwargs.items():
                 if key == '__class__':
                     continue
                 if key == 'created_at' or key == 'updated_at':
-                    value = datetime.fromisoformat(value)
+                    value = datetime.strptime(value, '%Y-%m-%dT%H:%M:%S.%f')
                 self.__dict__[key] = value
 
     def __str__(self):
