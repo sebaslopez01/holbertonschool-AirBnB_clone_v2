5e13c065c0370b4f7e9487128293aa78b40516fa
Sebastian <sebaslopez.2002@gmail.com>
2022-11-28 22:59:03 -0500

    setup database

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,4 @@
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'user_0d_1pwd';
+GRANT ALL PRIVELEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema TO 'hbnb_dev'@'localhost';
