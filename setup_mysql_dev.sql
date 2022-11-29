32b9870fc9bca6d981528b06f68742812a0fd618
Sebastian <sebaslopez.2002@gmail.com>
2022-11-28 22:59:36 -0500

    added comment

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,3 +1,4 @@
+-- Create database and user
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'user_0d_1pwd';
 GRANT ALL PRIVELEGES ON hbnb_dev_db TO 'hbnb_dev'@'localhost';
