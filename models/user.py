fba6b8143143c6f8cad33941573efbaa38a3b0ba
Sebastian <sebaslopez.2002@gmail.com>
2022-11-29 19:34:00 -0500

    user definition corrected

--- models/user.py
+++ models/user.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String
 
 
 class User(BaseModel, Base):
@@ -10,5 +10,5 @@ class User(BaseModel, Base):
 
     email = Column(String(128), nullable=False)
     password = Column(String(128), nullable=False)
-    first_name = Column(String(128), nullable=False)
-    last_name = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
