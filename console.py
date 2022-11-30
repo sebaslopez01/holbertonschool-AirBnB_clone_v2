0e1a403835f70ffd0844ed8719cea272d8781f3c
Sebastian <sebaslopez.2002@gmail.com>
2022-11-29 19:00:31 -0500

    print all models fixed

--- console.py
+++ console.py
@@ -223,7 +223,7 @@ class HBNBCommand(cmd.Cmd):
             if args not in HBNBCommand.classes:
                 print("** class doesn't exist **")
                 return
-            for k, v in storage.all().items():
+            for k, v in storage.all(args).items():
                 if k.split('.')[0] == args:
                     print_list.append(str(v))
         else:
