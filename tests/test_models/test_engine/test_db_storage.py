da46b58c6cef3f79f5ab72f88ef1c5ed96bebc71
Sebastian <sebaslopez.2002@gmail.com>
2022-12-02 17:47:18 -0500

    d

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -51,5 +51,5 @@ class TestDBStorage(unittest.TestCase):
         state = State(name='California')
         state.save()
         states = storage.all(State)
-        self.assertEqual(self.state, states['State'+'.'+self.state.id])
+        self.assertEqual(state, states['State'+'.'+self.state.id])
         storage.delete(state)
