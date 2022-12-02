dd549aa8ba03ccb1cb06e25d422e3cddc6c08bf0
Sebastian <sebaslopez.2002@gmail.com>
2022-12-02 18:44:06 -0500

    f

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -2,6 +2,7 @@
 """ Module for testing file storage"""
 import unittest
 from models.base_model import BaseModel
+from models.engine.db_storage import DBStorage
 from models import storage
 import os
 
@@ -24,10 +25,12 @@ class test_fileStorage(unittest.TestCase):
         except:
             pass
 
+    @unittest.skipIf(type(storage) == DBStorage, "DBStorage ignore")
     def test_obj_list_empty(self):
         """ __objects is initially empty """
         self.assertEqual(len(storage.all()), 0)
 
+    @unittest.skipIf(type(storage) == DBStorage, "DBStorage ignore")
     def test_new(self):
         """ New object is correctly added to __objects """
         new = BaseModel()
@@ -35,17 +38,20 @@ class test_fileStorage(unittest.TestCase):
         temp = storage.all()['BaseModel'+'.'+new.id]
         self.assertTrue(temp is new)
 
+    @unittest.skipIf(type(storage) == DBStorage, "DBStorage ignore")
     def test_all(self):
         """ __objects is properly returned """
         new = BaseModel()
         temp = storage.all()
         self.assertIsInstance(temp, dict)
 
+    @unittest.skipIf(type(storage) == DBStorage, "DBStorage ignore")
     def test_base_model_instantiation(self):
         """ File is not created on BaseModel save """
         new = BaseModel()
         self.assertFalse(os.path.exists('file.json'))
 
+    @unittest.skipIf(type(storage) == DBStorage, "DBStorage ignore")
     def test_empty(self):
         """ Data is saved to file """
         new = BaseModel()
@@ -54,12 +60,14 @@ class test_fileStorage(unittest.TestCase):
         new2 = BaseModel(**thing)
         self.assertNotEqual(os.path.getsize('file.json'), 0)
 
+    @unittest.skipIf(type(storage) == DBStorage, "DBStorage ignore")
     def test_save(self):
         """ FileStorage save method """
         new = BaseModel()
         storage.save()
         self.assertTrue(os.path.exists('file.json'))
 
+    @unittest.skipIf(type(storage) == DBStorage, "DBStorage ignore")
     def test_reload(self):
         """ Storage file is successfully loaded to __objects """
         new = BaseModel()
@@ -68,6 +76,7 @@ class test_fileStorage(unittest.TestCase):
         loaded = storage.all()['BaseModel'+'.'+new.id]
         self.assertEqual(new.to_dict()['id'], loaded.to_dict()['id'])
 
+    @unittest.skipIf(type(storage) == DBStorage, "DBStorage ignore")
     def test_reload_empty(self):
         """ Load from an empty file """
         with open('file.json', 'w') as f:
@@ -75,24 +84,29 @@ class test_fileStorage(unittest.TestCase):
         with self.assertRaises(ValueError):
             storage.reload()
 
+    @unittest.skipIf(type(storage) == DBStorage, "DBStorage ignore")
     def test_reload_from_nonexistent(self):
         """ Nothing happens if file does not exist """
         self.assertEqual(storage.reload(), None)
 
+    @unittest.skipIf(type(storage) == DBStorage, "DBStorage ignore")
     def test_base_model_save(self):
         """ BaseModel save method calls storage save """
         new = BaseModel()
         new.save()
         self.assertTrue(os.path.exists('file.json'))
 
+    @unittest.skipIf(type(storage) == DBStorage, "DBStorage ignore")
     def test_type_path(self):
         """ Confirm __file_path is string """
         self.assertEqual(type(storage._FileStorage__file_path), str)
 
+    @unittest.skipIf(type(storage) == DBStorage, "DBStorage ignore")
     def test_type_objects(self):
         """ Confirm __objects is a dict """
         self.assertEqual(type(storage.all()), dict)
 
+    @unittest.skipIf(type(storage) == DBStorage, "DBStorage ignore")
     def test_key_format(self):
         """ Key is properly formatted """
         new = BaseModel()
@@ -100,6 +114,7 @@ class test_fileStorage(unittest.TestCase):
         _id = new.to_dict()['id']
         self.assertEqual(storage.all()['BaseModel' + '.' + _id].id, _id)
 
+    @unittest.skipIf(type(storage) == DBStorage, "DBStorage ignore")
     def test_storage_var_created(self):
         """ FileStorage object storage created """
         from models.engine.file_storage import FileStorage
