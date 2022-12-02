4d189030695e44d48977604afe0760a855b8d7a7
Sebastian <sebaslopez.2002@gmail.com>
2022-12-02 16:28:07 -0500

    better console

--- tests/test_console.py
+++ tests/test_console.py
@@ -21,6 +21,30 @@ class TestConsole(unittest.TestCase):
         except IOError:
             pass
 
+    @unittest.skipIf(type(storage) == FileStorage, 'Skip FileStorage')
+    def test_create_db(self):
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd('create State name="California"')
+            res = f.getvalue().strip()
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd(f'create City state_id="{res}" name="Fremont"')
+            res2 = f.getvalue().strip()
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd(
+                f'create City state_id="{res}" name="San_Francisco"')
+            res3 = f.getvalue().strip()
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd('all State')
+            self.assertIn(res, f.getvalue().strip())
+
+        with patch('sys.stdout', new=StringIO()) as f:
+            self.console.onecmd('all City')
+            self.assertIn(res2, f.getvalue().strip())
+            self.assertIn(res3, f.getvalue().strip())
+
     @unittest.skipIf(type(storage) == DBStorage, 'Skip DbStorage')
     def test_create(self):
         with patch('sys.stdout', new=StringIO()) as f:
@@ -32,7 +56,8 @@ class TestConsole(unittest.TestCase):
             res2 = f.getvalue().strip()
 
         with patch('sys.stdout', new=StringIO()) as f:
-            self.console.onecmd(f'create City state_id="{res2}" name="Fremont')
+            self.console.onecmd(
+                f'create City state_id="{res2}" name="Fremont"')
             res3 = f.getvalue().strip()
 
         with patch('sys.stdout', new=StringIO()) as f:
