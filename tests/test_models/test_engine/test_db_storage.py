046fb68648f7a8ff295d1569742837c98551d4fe
Sebastian <sebaslopez.2002@gmail.com>
2022-12-02 17:45:31 -0500

    dn

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -14,39 +14,42 @@ from models import storage
 
 
 class TestDBStorage(unittest.TestCase):
-    def setUp(self) -> None:
-        if type(storage) == DBStorage:
-            self.storage = storage
-            self.user = User(email='none@gmail.com', password='password')
-            self.user.save()
-            self.state = State(name='California')
-            self.state.save()
-            self.city = City(name='San Francisco', state_id=self.state.id)
-            self.city.save()
-            self.place = Place(user_id=self.user.id,
-                               city_id=self.city.id, name='Church')
-            self.place.save()
-            self.review = Review(user_id=self.user.id,
-                                 palce_id=self.place.id, text='Very good')
-            self.review.save()
-            self.amenity = Amenity(name='TV')
-            self.amenity.save()
+    # def setUp(self) -> None:
+    #     if type(storage) == DBStorage:
+    #         self.storage = storage
+    #         self.user = User(email='none@gmail.com', password='password')
+    #         self.user.save()
+    #         self.state = State(name='California')
+    #         self.state.save()
+    #         self.city = City(name='San Francisco', state_id=self.state.id)
+    #         self.city.save()
+    #         self.place = Place(user_id=self.user.id,
+    #                            city_id=self.city.id, name='Church')
+    #         self.place.save()
+    #         self.review = Review(user_id=self.user.id,
+    #                              palce_id=self.place.id, text='Very good')
+    #         self.review.save()
+    #         self.amenity = Amenity(name='TV')
+    #         self.amenity.save()
 
-    def tearDown(self) -> None:
-        if type(storage) == DBStorage:
-            self.storage.delete(self.user)
-            self.storage.delete(self.state)
-            self.storage.delete(self.city)
-            self.storage.delete(self.place)
-            self.storage.delete(self.review)
-            self.storage.delete(self.amenity)
-            self.storage.save()
+    # def tearDown(self) -> None:
+    #     if type(storage) == DBStorage:
+    #         self.storage.delete(self.user)
+    #         self.storage.delete(self.state)
+    #         self.storage.delete(self.city)
+    #         self.storage.delete(self.place)
+    #         self.storage.delete(self.review)
+    #         self.storage.delete(self.amenity)
+    #         self.storage.save()
 
     @unittest.skipIf(type(storage) == FileStorage, "File Storage ignore")
     def test_instance_creation(self):
-        self.assertEqual(type(self.storage), DBStorage)
+        self.assertEqual(type(storage), DBStorage)
 
     @unittest.skipIf(type(storage) == FileStorage, "File Storage ignore")
     def test_all(self):
-        states = self.storage.all(State)
+        state = State(name='California')
+        state.save()
+        states = storage.all(State)
         self.assertEqual(self.state, states['State'+'.'+self.state.id])
+        storage.delete(state)
