fbd0153966d3be991a29586e1dd2b51cb645baba
Sebastian <sebaslopez.2002@gmail.com>
2022-12-02 18:53:31 -0500

    w

--- models/amenity.py
+++ models/amenity.py
@@ -12,4 +12,4 @@ class Amenity(BaseModel, Base):
     name = Column(String(128), nullable=False)
     place_amenities = relationship(
         'Place', secondary='place_amenity',
-        back_populates='amenities', viewonly=False)
+        viewonly=False)
--- models/place.py
+++ models/place.py
@@ -33,7 +33,7 @@ class Place(BaseModel, Base):
     reviews = relationship('Review', backref='place', cascade='all, delete')
     amenities = relationship(
         'Amenity', secondary='place_amenity',
-        back_populates='place_amenities', viewonly=False)
+        viewonly=False)
     amenity_ids = []
 
     if os.getenv('HBNB_TYPE_STORAGE') != 'db':
