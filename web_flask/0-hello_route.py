44e93416811fdda14b8f3c9d76801ff8f57e802b
Sebastian <sebaslopez.2002@gmail.com>
2022-12-05 11:18:00 -0500

    better

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -3,7 +3,12 @@
 """This module defines a Flask app"""
 
 
-from web_flask import app
+from flask import Flask
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000)
 
 
 @app.route('/', strict_slashes=False)
