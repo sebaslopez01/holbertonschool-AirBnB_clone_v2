b8f985697e0a6abd7f9faad88cfeec266f644cd7
Sebastian <sebaslopez.2002@gmail.com>
2022-12-05 11:19:27 -0500

    fixed flask

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,10 +7,11 @@ from flask import Flask
 
 app = Flask(__name__)
 
-if __name__ == '__main__':
-    app.run('0.0.0.0', 5000)
-
 
 @app.route('/', strict_slashes=False)
 def hello():
     return 'Hello HBNB!'
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000)
