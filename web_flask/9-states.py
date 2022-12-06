d4394e550a8de282eafa333d41d11d4498da0c4b
Sebastian <sebaslopez.2002@gmail.com>
2022-12-06 12:09:02 -0500

    states

---
+++ web_flask/9-states.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+
+"""This module defines a Flask app with dynamic routes"""
+
+
+from flask import Flask, render_template
+from models import storage
+from models.state import State
+
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
+
+@app.route('/states', defaults={'id': None})
+@app.route('/states/<id>')
+def states_list(id):
+    if id is None:
+        states = storage.all(State)
+        state = None
+    else:
+        states = None
+        state = storage.all(State)['State'+'.'+id]
+    return render_template('9-states.html', states=states,
+                           state=state, error=False)
+
+
+@app.errorhandler(404)
+def not_found():
+    return render_template('9-states.html', states=None,
+                           state=None, error=True)
+
+
+@app.teardown_appcontext
+def close_conn(exception):
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000)
