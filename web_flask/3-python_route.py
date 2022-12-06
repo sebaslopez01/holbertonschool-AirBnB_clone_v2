c6b6759e20247a07a45cb2f20162249c10e6e8f4
Sebastian <sebaslopez.2002@gmail.com>
2022-12-05 19:26:14 -0500

    python route added

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+
+"""This module defines a Flask app with dynamic routes"""
+
+
+from flask import Flask
+from markupsafe import escape
+
+app = Flask(__name__)
+
+app.url_map.strict_slashes = False
+
+
+@app.route('/')
+def hello():
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb')
+def hbnb():
+    return 'HBNB'
+
+
+@app.route('/c/<text>')
+def c_route(text):
+    text = text.replace('_', ' ')
+
+    return f'C {escape(text)}'
+
+
+@app.route('/python/<text>')
+def python_route(text='is cool'):
+    text = text.replace('_', ' ')
+
+    return f'Python {escape(text)}'
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', 5000)
