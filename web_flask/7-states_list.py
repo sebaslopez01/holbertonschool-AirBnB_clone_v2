38f555dbc45022ed20c6aeda7fdf6527e57f114d
Sebastian <sebaslopez.2002@gmail.com>
2022-12-06 10:31:10 -0500

    f

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -5,20 +5,21 @@
 
 from flask import Flask, render_template
 from models import storage
+from models.state import State
 
 app = Flask(__name__)
 
 app.url_map.strict_slashes = False
 
 
+@app.route('/states_list')
+def states_list():
+    return render_template('7-states_list.html', states=storage.all(State))
+
+
 @app.teardown_appcontext
 def close_conn():
     storage.close()
-
-
-@app.route('/states_list')
-def states_list():
-    return render_template('7-states_list.html', states=storage.all('State'))
 
 
 if __name__ == '__main__':
