067207c8107ca8fbecb6052d97d4098d674e56f0
Sebastian <sebaslopez.2002@gmail.com>
2022-12-05 19:44:45 -0500

    fixed defaults route

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -28,7 +28,8 @@ def c_route(text):
     return f'C {escape(text)}'
 
 
-@app.route('/python/<text>', defaults={'text': 'is cool'})
+@app.route('/python/', defaults={'text': 'is cool'})
+@app.route('/python/<text>')
 def python_route(text):
     text = text.replace('_', ' ')
 
