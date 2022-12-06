948defe06142a7aa491033709aef8c57dd17ae19
Sebastian <sebaslopez.2002@gmail.com>
2022-12-05 23:01:09 -0500

    fixed

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -65,7 +65,3 @@ class DBStorage:
 
     def close(self):
         self.__session.close()
-        session_factory = sessionmaker(
-            bind=self.__engine, expire_on_commit=False)
-        Session = scoped_session(session_factory)
-        self.__session = Session()
