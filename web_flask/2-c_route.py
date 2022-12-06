2301afdd648e7b52f7e33d65e3c30a54e6f24a20
Sebastian <sebaslopez.2002@gmail.com>
2022-12-05 19:50:40 -0500

    d

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -14,22 +14,20 @@ from markupsafe import escape
 
 app = Flask(__name__)
 
-app.url_map.strict_slashes = False
 
-
-@app.route('/')
+@app.route('/', strict_slashes=False)
 def hello():
     """Prints 'Hello HBNB!'"""
     return 'Hello HBNB!'
 
 
-@app.route('/hbnb')
+@app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """Prints 'HBNB'"""
     return 'HBNB'
 
 
-@app.route('/c/<text>')
+@app.route('/c/<text>', strict_slashes=False)
 def c_route(text):
     """Prints 'C' followed by a <text>  """
     text = text.replace('_', ' ')
