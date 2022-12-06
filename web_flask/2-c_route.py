60af06d41387c4f6d7dd04c73d4249e54d3ded69
Sebastian <sebaslopez.2002@gmail.com>
2022-12-05 19:57:54 -0500

    d

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """This module defines a Flask app with dynamic routes 
 
+This application listen in localhost port 5000.
 Routes:
     /
     /hbnb
@@ -13,19 +14,19 @@ app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def hello():
-    """Prints 'Hello HBNB!'"""
+    """Prints 'Hello HBNB!'."""
     return 'Hello HBNB!'
 
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """Prints 'HBNB'"""
+    """Prints 'HBNB'."""
     return 'HBNB'
 
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
-    """Prints 'C' followed by a <text>  """
+    """Prints 'C' followed by a <text>."""
     text = text.replace('_', ' ')
 
     return f'C {text}'
