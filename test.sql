85b5fdbecef467f204146d90b75f20788817ffc9
Sebastian <sebaslopez.2002@gmail.com>
2022-12-06 08:29:44 -0800

    s

--- test.sql
+++
@@ -1,4 +0,0 @@
-USE hbnb_dev_db;
-INSERT INTO states (id, name, created_at, updated_at) VALUES ("my_id_a", "Arizona", CURDATE(), CURDATE());
-INSERT INTO states (id, name, created_at, updated_at) VALUES ("my_id_n", "New York", CURDATE(), CURDATE());
-INSERT INTO states (id, name, created_at, updated_at) VALUES ("my_id_i", "Illinois", CURDATE(), CURDATE());
