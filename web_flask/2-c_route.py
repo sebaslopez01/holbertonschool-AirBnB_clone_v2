b3008c59c15deac9bb289e771cc4382b9bcd4c7a
Sebastian <sebaslopez.2002@gmail.com>
2022-12-05 20:00:13 -0500

    d

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -12,25 +12,24 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
+@app.route("/", strict_slashes=False)
 def hello():
     """Prints 'Hello HBNB!'."""
-    return 'Hello HBNB!'
+    return "Hello HBNB!"
 
 
-@app.route('/hbnb', strict_slashes=False)
+@app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Prints 'HBNB'."""
-    return 'HBNB'
+    return "HBNB"
 
 
-@app.route('/c/<text>', strict_slashes=False)
+@app.route("/c/<text>", strict_slashes=False)
 def c(text):
     """Prints 'C' followed by a <text>."""
-    text = text.replace('_', ' ')
+    text = text.replace("_", " ")
+    return f"C {text}"
 
-    return f'C {text}'
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run('0.0.0.0', 5000)
