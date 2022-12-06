f8cfcb45a990bc17fda603b92a26566c48ed2bd5
Sebastian <sebaslopez.2002@gmail.com>
2022-12-05 20:03:02 -0500

    final

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,30 +1,27 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
 
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /: Displays 'Hello HBNB!'.
-    /hbnb: Displays 'HBNB'.
-    /c/<text>: Displays 'C' followed by the value of <text>.
-"""
+"""This modules defines a Flask app"""
+
 from flask import Flask
 
 app = Flask(__name__)
 
+app.url_map.strict_slashes = False
 
-@app.route("/", strict_slashes=False)
+
+@app.route("/")
 def hello():
     """Prints 'Hello HBNB!'."""
     return "Hello HBNB!"
 
 
-@app.route("/hbnb", strict_slashes=False)
+@app.route("/hbnb")
 def hbnb():
     """Prints 'HBNB'."""
     return "HBNB"
 
 
-@app.route("/c/<text>", strict_slashes=False)
+@app.route("/c/<text>")
 def c(text):
     """Prints 'C' followed by a <text>."""
     text = text.replace("_", " ")
