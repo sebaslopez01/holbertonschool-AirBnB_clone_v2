96b06179932be5abfd8264bc85f0621662d4c3bd
Sebastian <sebaslopez.2002@gmail.com>
2022-12-06 08:57:04 -0500

    fixed

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -27,7 +27,7 @@ class DBStorage:
         host = os.getenv("HBNB_MYSQL_HOST")
         db = os.getenv("HBNB_MYSQL_DB")
         self.__engine = create_engine(
-            f'mysql+mysqldb://{user}:{pwd}@{host}/{db}',
+            'mysql+mysqldb://{}:{}@{}/{}'.format(user, pwd, host, db),
             pool_pre_ping=True)
         if os.getenv('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
